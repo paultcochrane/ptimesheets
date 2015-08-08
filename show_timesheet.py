5bd8640525cc7b96d3b281a72e5ca519c661e83d
Paul Cochrane <paul@liekut.de>
2015-08-08 15:37:52 +0200

    Add --timesheet option to show_timesheet.py
    Now one can specify any timesheet to process instead of just aaw.yml

--- show_timesheet.py
+++ show_timesheet.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
 
 import yaml
+import argparse
 
 from timesheets.timesheet import Timesheet
 
-yml_fh = open("aaw.yml")
+parser = argparse.ArgumentParser(description='Convert timesheets to CSV')
+parser.add_argument('--timesheet', type=str, required=True,
+                        help='timesheet file name')
+args = parser.parse_args()
+timesheet_yml_fname = args.timesheet
+
+yml_fh = open(timesheet_yml_fname)
 timesheet_items = yaml.load_all(yml_fh)
 
 timesheet = Timesheet()
